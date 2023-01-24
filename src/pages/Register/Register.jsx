c79901a9189e0cc911232b7bb18b1721bf49162f
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2023-01-24 10:48:11 +0200

    added adaptive registration

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -9,9 +9,10 @@ import {
   RegPassWord,
   LinkToLog,
   ContLabel,
-  Container,
+  Wrap,
   TitleReg,
 } from './Register.styled';
+import { Container } from 'components/Container';
 import { register } from 'Redux/Authorization/operations';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -31,101 +32,103 @@ export default function Register() {
   return (
     <section>
       <Container>
-        <FormReg
-          form={form}
-          name="register"
-          onFinish={onFinish}
-          initialValues={{
-            residence: ['zhejiang', 'hangzhou', 'xihu'],
-            prefix: '86',
-          }}
-          scrollToFirstError
-        >
-          <TitleReg>Register new user</TitleReg>
-          <Label
-            name="name"
-            label="Name"
-            tooltip="What do you want others to call you?"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your name!',
-                whitespace: true,
-              },
-            ]}
+        <Wrap>
+          <FormReg
+            form={form}
+            name="register"
+            onFinish={onFinish}
+            initialValues={{
+              residence: ['zhejiang', 'hangzhou', 'xihu'],
+              prefix: '86',
+            }}
+            scrollToFirstError
           >
-            <InputReg prefix={<RegUser />} />
-          </Label>
-          <Label
-            name="email"
-            label="E-mail"
-            rules={[
-              {
-                type: 'email',
-                message: 'The input is not valid E-mail!',
-              },
-              {
-                required: true,
-                message: 'Please input your E-mail!',
-              },
-            ]}
-          >
-            <InputReg prefix={<RegMail />} />
-          </Label>
-          <Label
-            name="password"
-            label="Password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-            hasFeedback
-          >
-            <InputPassword
-              prefix={<RegPassWord />}
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}"
-              title="Must contain at least one number and one uppercase and lowercase letter, and at least 7 or more characters"
-            />
-          </Label>
-          <Label
-            name="confirm"
-            label="Confirm Password"
-            dependencies={['password']}
-            hasFeedback
-            rules={[
-              {
-                required: true,
-                message: 'Please confirm your password!',
-              },
-              ({ getFieldValue }) => ({
-                validator(_, value) {
-                  if (!value || getFieldValue('password') === value) {
-                    return Promise.resolve();
-                  }
-                  return Promise.reject(
-                    new Error(
-                      'The two passwords that you entered do not match!'
-                    )
-                  );
+            <TitleReg>Register new user</TitleReg>
+            <Label
+              name="name"
+              label="Name"
+              tooltip="What do you want others to call you?"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your name!',
+                  whitespace: true,
                 },
-              }),
-            ]}
-          >
-            <InputPassword prefix={<RegPassWord />} />
-          </Label>
-          <Label>
-            {' '}
-            <SubmitButton type="primary" htmlType="submit">
-              Register
-            </SubmitButton>{' '}
-            <ContLabel>
-              or <LinkToLog to="/login"> log in</LinkToLog>if you already have
-              an account{' '}
-            </ContLabel>
-          </Label>
-        </FormReg>
+              ]}
+            >
+              <InputReg prefix={<RegUser />} />
+            </Label>
+            <Label
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
+              <InputReg prefix={<RegMail />} />
+            </Label>
+            <Label
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+              hasFeedback
+            >
+              <InputPassword
+                prefix={<RegPassWord />}
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{7,}"
+                title="Must contain at least one number and one uppercase and lowercase letter, and at least 7 or more characters"
+              />
+            </Label>
+            <Label
+              name="confirm"
+              label="Confirm Password"
+              dependencies={['password']}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: 'Please confirm your password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        'The two passwords that you entered do not match!'
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <InputPassword prefix={<RegPassWord />} />
+            </Label>
+            <Label>
+              {' '}
+              <SubmitButton type="primary" htmlType="submit">
+                Register
+              </SubmitButton>{' '}
+              <ContLabel>
+                or <LinkToLog to="/login"> log in</LinkToLog>if you already have
+                an account{' '}
+              </ContLabel>
+            </Label>
+          </FormReg>
+        </Wrap>
       </Container>
     </section>
   );
--- src/pages/Register/Register.styled.jsx
+++ src/pages/Register/Register.styled.jsx
@@ -4,23 +4,37 @@ import { UserOutlined, LockOutlined, MailOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 
 export const Label = styled(Form.Item)`
+  margin-bottom: 10px;
+  @media (min-width: 768px) {
+    margin-bottom: 24px;
+  }
+
   label {
     width: 150px;
     padding-top: 8px;
   }
 `;
-export const Container = styled.div`
+export const Wrap = styled.div`
   padding-top: 10%;
-  display: flex;
-  padding-left: 20%;
+  @media (min-width: 768px) {
+    padding-left: 10%;
+  }
+  @media (min-width: 1280px) {
+    padding-left: 25%;
+  }
 `;
 export const FormReg = styled(Form)`
-  width: 500px;
+  width: 100%;
+  @media (min-width: 768px) {
+    width: 500px;
+  }
 `;
 export const SubmitButton = styled(Button)`
   width: 100px;
   height: 40px;
-  margin-left: 30%;
+  @media (min-width: 768px) {
+    margin-left: 30%;
+  }
   display: block;
   font-size: 18px;
 `;
@@ -51,10 +65,16 @@ export const ContLabel = styled.div`
   position: absolute;
   top: -1%;
   left: 50%;
-  margin-left: 60px;
+  @media (min-width: 768px) {
+    margin-left: 60px;
+  }
 `;
 export const TitleReg = styled.h2`
-  margin-left: 30%;
-  margin-bottom: 15px;
   color: #2e2d2dbc;
+  margin-bottom: 15px;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    margin-left: 30%;
+  }
 `;
